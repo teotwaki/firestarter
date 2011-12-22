8bef8acdcf2602461ff3a6a141b4945ba7383b71
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2011-12-22 18:56:04 +0100

    Disable logging when in unit test

--- src/common/log.h
+++ src/common/log.h
@@ -7,27 +7,49 @@
 #include <cstdlib>
 #include <cstring>
 
-#define DECLARE_LOG(var, name) \
-	log4cxx::LoggerPtr var(log4cxx::Logger::getLogger(name))
+#ifndef IN_UNIT_TESTING
+	#define DECLARE_LOG(var, name) \
+		log4cxx::LoggerPtr var(log4cxx::Logger::getLogger(name))
+	
+	#define LOG_TRACE(logger, msg)	LOG4CXX_TRACE(logger, msg)
+	#define LOG_T(msg)				LOG4CXX_TRACE(logger, msg)
+	
+	#define LOG_DEBUG(logger, msg)	LOG4CXX_DEBUG(logger, msg)
+	#define LOG_D(msg)				LOG4CXX_DEBUG(logger, msg)
+	
+	#define LOG_ERROR(logger, msg)	LOG4CXX_ERROR(logger, msg)
+	#define LOG_E(msg)				LOG4CXX_ERROR(logger, msg)
+	
+	#define LOG_FATAL(logger, msg)	LOG4CXX_FATAL(logger, msg)
+	#define LOG_F(msg)				LOG4CXX_FATAL(logger, msg)
+	
+	#define LOG_INFO(logger, msg)	LOG4CXX_INFO(logger, msg)
+	#define LOG_I(msg)				LOG4CXX_INFO(logger, msg)
+	
+	#define LOG_WARN(logger, msg)	LOG4CXX_WARN(logger, msg)
+	#define LOG_W(msg)				LOG4CXX_WARN(logger, msg)
 
-#define LOG_TRACE(logger, msg)	LOG4CXX_TRACE(logger, msg)
-#define LOG_T(msg)				LOG4CXX_TRACE(logger, msg)
-
-#define LOG_DEBUG(logger, msg)	LOG4CXX_DEBUG(logger, msg)
-#define LOG_D(msg)				LOG4CXX_DEBUG(logger, msg)
-
-#define LOG_ERROR(logger, msg)	LOG4CXX_ERROR(logger, msg)
-#define LOG_E(msg)				LOG4CXX_ERROR(logger, msg)
-
-#define LOG_FATAL(logger, msg)	LOG4CXX_FATAL(logger, msg)
-#define LOG_F(msg)				LOG4CXX_FATAL(logger, msg)
-
-#define LOG_INFO(logger, msg)	LOG4CXX_INFO(logger, msg)
-#define LOG_I(msg)				LOG4CXX_INFO(logger, msg)
-
-#define LOG_WARN(logger, msg)	LOG4CXX_WARN(logger, msg)
-#define LOG_W(msg)				LOG4CXX_WARN(logger, msg)
-
+#else
+	#define DECLARE_LOG(var, name) 
+	
+	#define LOG_TRACE(logger, msg)
+	#define LOG_T(msg)
+	
+	#define LOG_DEBUG(logger, msg)
+	#define LOG_D(msg)
+	
+	#define LOG_ERROR(logger, msg)
+	#define LOG_E(msg)
+	
+	#define LOG_FATAL(logger, msg)
+	#define LOG_F(msg)
+	
+	#define LOG_INFO(logger, msg)
+	#define LOG_I(msg)
+	
+	#define LOG_WARN(logger, msg)
+	#define LOG_W(msg)
+#endif
 void set_logfile_name(std::string name);
 
 #endif
