ca62b8e13b9b4e0dcc6742fa08702b1556e8b219
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2012-02-02 09:51:01 +0100

    Added code example to SimpleCache documentation

--- src/common/simplecache.h
+++ src/common/simplecache.h
@@ -7,13 +7,41 @@
   * The template argument is the data type that needs to be cached. The data gets deleted automatically when it is invalidated or when the object is destroyed.
   * SimpleCache is mainly used by DependencyGraph.
   *
+  * This class can be used both through inheritance or composition.
+  * Inheritance example:
+  * \code
+  * // Let's assume there is a class called BigDataType:
+  * class MyExample : private SimpleCache<BigDataType> {
+  *     public:
+  *     MyExample() : SimpleCache<BigDataType>() { this->initCache(); };
+  *     insertData(Data foo) { this->getCache()->add(foo); };
+  *     deleteData() { this->invalidateCache(); };
+  *     // ...
+  * };
+  * \endcode
+  * It is possible to override any of the methods in order to adapt the structure initialisation / assignment to your needs.
+  *
+  * Object composition example:
+  * \code
+  * class MySecondExample {
+  *     private:
+  *     SimpleCache<BigDataType> cache;
+  *
+  *     public:
+  *     insertData(Data foo) { if (not this->cache->cacheIsValid()) this->cache->initCache(); this->cache->getCache()->add(foo); };
+  *     deleteData() { this->cache->invalidateCache(); };
+  *     // ...
+  * };
+  * \endcode
+  *
   * \see For an example implementation, see DependencyGraph
   */
 template<typename T> class SimpleCache {
-	public:
+	protected:
 	/// \briefThe pointer to the data that will be held
 	T * cache;
 
+	public:
 	/** \brief Initialises the object with no cache
 	  *
 	  * Call initCache() to initialise the internal cache and storage.
@@ -32,7 +60,7 @@ template<typename T> class SimpleCache {
 	  * This function should not be used directly. Rather, use initCache() to instantiate the data.
 	  * \see initCache
 	  */
-	void setCache(/** [in] */ T * _cache) { this->cache = _cache; };
+	virtual void setCache(/** [in] */ T * _cache) { this->cache = _cache; };
 	/** \brief Check if the cache is in a valid state or not
 	  *
 	  * \return true if the cache is initialised, false otherwise
