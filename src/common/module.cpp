5ce9d741a1457c0a9b91550af96851187c4b5bdf
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2012-02-08 20:31:19 +0100

    Added RunnableModule class

---
+++ src/common/module.cpp
@@ -0,0 +1,9 @@
+#include "module.h"
+
+void RunnableModule::send(google::protobuf::Message & pb_message, zmq::socket_t * socket) {
+	std::string pb_serialised;
+	pb_message.SerializeToString(&pb_serialised);
+	zmq::message_t message(pb_serialised.size());
+	memcpy(static_cast<void *>(message.data()), pb_serialised.c_str(), pb_serialised.size());
+	socket->send(message);
+}	
--- src/common/module.h
+++ src/common/module.h
@@ -6,13 +6,32 @@
 #include <boost/tr1/unordered_map.hpp>
 
 #include "helper.h"
+#include "zmqhelper.h"
+#include "protobuf/module.pb.h"
 
 class Module {
 	protected:
-	/* Properties */
 
 	public:
-	virtual void setup() = 0; /* pure virtual */
+	virtual void setup() = 0; /**< pure virtual */
+};
+
+class RunnableModule : public Module {
+	protected:
+	bool running;
+	zmq::context_t * context;
+	zmq::socket_t * manager;
+
+	inline void createManagementSocket() { 
+		this->manager = new zmq::socket_t(*(this->context), ZMQ_REP); 
+		this->manager->connect(MANAGER_SOCKET_URI); 
+	};
+	virtual void send(google::protobuf::Message & pb_message, zmq::socket_t * socket);
+
+	public:
+	RunnableModule();
+	void setContext(zmq::context_t * context) { this->context = context; };
+	
 };
 
 typedef Module * create_module();
