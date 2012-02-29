9254b363f22f510b761928268aef066a2d6f237c
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2012-02-29 18:04:28 +0100

    Implemented client socket for instancemanager

---
+++ src/common/clients/instancemanager.h
@@ -0,0 +1,31 @@
+#ifndef __CLIENT_INSTANCEMANAGER_H
+#define __CLIENT_INSTANCEMANAGER_H
+
+#include "zmq/zmqsocket.h"
+#include "protobuf/module.pb.h"
+
+namespace firestarter {
+	namespace InstanceManager {
+
+class InstanceManagerClientSocket {
+	private:
+	firestarter::sockets::ZMQSubscriberSocket subscriber;
+	firestarter::sockets::ZMQRequestSocket requester;
+
+	public:
+	InstanceManagerClientSocket(zmq::context_t & context) :
+									subscriber(context, MODULE_ORDERS_SOCKET_URI), requester(context, MANAGER_SOCKET_URI) { };
+	inline bool send(google::protobuf::Message & pb_message) { 
+		if (this->requester.send(pb_message))
+			return this->receive_ack();
+		return false;
+	};
+	inline bool receive(google::protobuf::Message & pb_message, bool blocking = false) { return this->subscriber.receive(pb_message, blocking); };
+	inline bool receive_ack() { return this->requester.receive(); };
+};
+
+/* Closing namespaces */
+	}
+}
+
+#endif
