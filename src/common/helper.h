dd5dcec74ff658b7e69675051664b927e46b0240
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2012-02-29 17:59:01 +0100

    Moved foreach definition out of helper

--- src/common/helper.h
+++ src/common/helper.h
@@ -1,25 +1,14 @@
 #ifndef __HELPER_H
 #define __HELPER_H
 
+#include "foreach.h"
+
 #include <string>
 #include <sys/stat.h>
 
 #include "log.h"
 #include "module.h"
 #include "exceptions.h"
-
-// Prettify BOOST's foreach.
-#ifndef foreach
-	#define foreach BOOST_FOREACH
-#endif
-#ifndef reverse_foreach
-	#define reverse_foreach BOOST_FOREACH_REVERSE
-#endif
-
-// There is a bug in boost 1.48 which requires foreach to be defined
-// before including foreach.hpp
-
-#include <boost/foreach.hpp>
 
 bool file_exists(std::string path);
 
