8ceb1ec856ea29adb85de1200ef6067e85bbd027
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2012-03-04 03:23:17 +0100

    Further documentation of ZMQ*Sockets

--- src/common/zmq/zmqsocket.h
+++ src/common/zmq/zmqsocket.h
@@ -36,6 +36,12 @@ class ZMQSocket {
 	/** \brief The constructor simply initialises the pointer to NULL */
 	ZMQSocket() : socket(NULL) { };
 
+	/** \brief Destroy the object and the underlying socket if it exists */
+	~ZMQSocket() {
+		if (this->socket != NULL)
+			delete this->socket;
+	};
+
 	public:
 	/** \brief Return a pointer that can be used with zmq_poll() */
 	inline void * pollable() { return static_cast<void *>(this->socket); };
@@ -127,10 +133,6 @@ class ZMQSendingSocket : virtual public ZMQSocket {
 	  * message the server needs to send a message, even an empty one.
 	  *
 	  * \return True when the message was sent, false otherwise.
-	  *
-	  * \param send_more When set to true, the function indicates to the ZMQ layer that
-	  * the current message is a multipart one. A multipart message enables the segmentation
-	  * of messages, that will all be sent and delivered at once in an atomical manner.
 	  */
 	bool send();
 	/** \brief Send a protobuf message
@@ -147,6 +149,8 @@ class ZMQSendingSocket : virtual public ZMQSocket {
 	/** \brief Bind the socket to a local endpoint for incoming connections
 	  *
 	  * This method binds the socket to a specific endpoint for other sockets to connect to.
+	  * If the string has a zero-length, no binding will be done, and the method will silently
+	  * fail.
 	  *
 	  * \param uri A reference to a string containing the endpoint to which the socket
 	  * should bind.
@@ -159,7 +163,8 @@ class ZMQSendingSocket : virtual public ZMQSocket {
 	/** \brief Bind the socket to a list of local endpoint for incoming connections
 	  *
 	  * This is basically a wrapper around the regular bind() method, enabling the user
-	  * to bind a number of endpoints in a single call.
+	  * to bind a number of endpoints in a single call. If any of the strings are zero-length,
+	  * it will simply be ignored. However, the other items in the list will still be processed.
 	  *
 	  * \param uris A reference to a list of strings containing the endpoints to which the
 	  * socket should bind.
@@ -171,34 +176,147 @@ class ZMQSendingSocket : virtual public ZMQSocket {
 	};
 };
 
+/** \brief A publisher (fan-out) send-only socket
+  *
+  * A publisher socket provides a one-to-many (fan-out) sending socket. It is capable of
+  * sending data to multiple subscribers, but however is unable to receive any data, which
+  * is why it only implements the ZMQSendingSocket class. Subscribers should use the
+  * ZMQSubscriberSocket class.
+  *
+  * Messages are sent out to all connected subscribers simultaneously (with the obvious
+  * caveats linked to network delays and lost packets). If a specific subscriber is unable
+  * to process all data and the connection to that specific subscriber reaches the ZMQ
+  * high water mark, all messages will be dropped until the exceptional state ends.
+  *
+  * According to the ZMQ specification, send() is guaranteed to never block for this kind
+  * of socket.
+  *
+  * \see ZMQSendingSocket
+  * \see ZMQSubscriberSocket
+  */
 class ZMQPublisherSocket : public ZMQSendingSocket {
 	public:
-	ZMQPublisherSocket(zmq::context_t & context, std::string & uri = "") { this->socket = new zmq::socket_t(context, ZMQ_PUB); this->bind(uri); };
+	/** \brief Constructor which initialises the underlying socket
+	  *
+	  * The constructor initialises the underlying ZMQ socket by using the provided ZMQ
+	  * context. If a uri is provided, the socket will bind to it. The socket is usable
+	  * immediately (if a uri has been provided).
+	  *
+	  * \param context A reference to the ZMQ context in which the socket should be created.
+	  * \param uri Optional reference to a string on which the socket should bind.
+	  **/
+	ZMQPublisherSocket(/** [in] */ zmq::context_t & context, /** [in] */ std::string & uri = "") { 
+		this->socket = new zmq::socket_t(context, ZMQ_PUB); 
+		this->bind(uri); 
+	};
+	/** \brief Constructor which initialises the underlying socket and binds to a list of uris
+	  *
+	  * This constructor provides the same functionalities as the other one, with the
+	  * exception that it requires a list of uris to which the socket should bind to.
+	  *
+	  * \param context A reference to the ZMQ context in which the socket should be created.
+	  * \param uris A reference to a list of strings on which the socket should bind.
+	  */
+	ZMQPublisherSocket(/** [in] */ zmq::context_t & context, /** [in] */ std::list<std::string> & uris) {
+		this->socket = new zmq::socket_t(context, ZMQ_PUB);
+		this->bind(uris);
+	};
 };
 
+/** \brief A subscriber (fan-in) receive-only socket
+  *
+  * A subscriber sockets provides the capability to subscribe to one or multiple publisher
+  * sockets. It is capable of receiving data from multiple publishers, but is however unable
+  * to send any data, which is why it only implements the ZMQReceivingSocket class. Publishers
+  * should use the ZMQPublisherSocket class.
+  *
+  * Messages are received from all connected publishers by using a fair-queued routing strategy.
+  * If the subscriber is unable to process all the messages coming from publishers, the publishers
+  * will eventually reach their high water mark and begin dropping packets.
+  *
+  * \see ZMQReceivingSocket
+  * \see ZMQPublisherSocket
+  */
 class ZMQSubscriberSocket : public ZMQReceivingSocket {
 	public:
-	ZMQSubscriberSocket(zmq::context_t & context, std::string & uri = "") { 
+	/** \brief Constructor which initialises the underlying socket
+	  *
+	  * The constructor initialises the underlying ZMQ socket by using the provided ZMQ
+	  * context. If a uri is provided, the socket will connect to it. The socket is usable
+	  * immediately (if a uri has been provided).
+	  *
+	  * By default, the socket will subscribe to any kind of message, unless the third argument
+	  * is set to false.
+	  *
+	  * \see connect()
+	  * \see subscribe()
+	  *
+	  * \param context A reference to the ZMQ context in which the socket should be created.
+	  * \param uri Optional reference to a string on which the socket should bind.
+	  * \param [in] subscribe If set to true, the socket will automatically subscribe to any kind
+	  * of messages. If set to false, no subscription will be done.
+	  */
+	ZMQSubscriberSocket(/** [in] */ zmq::context_t & context, /** [in] */ std::string & uri = "", 
+						/** [in] */ bool subscribe = true) { 
 		this->socket = new zmq::socket_t(context, ZMQ_SUB); 
-		this->connect(uri); 
-		this->subscribe();
+		this->connect(uri);
+		if (subscribe)
+			this->subscribe();
 	};
-	inline void subscribe(std::string & filter = "") { 
+	/** \brief Constructor which initialises the underlying socket and connects to a list of uris
+	  *
+	  * This constructor provides the same functionalities as the other one, with the
+	  * exception that it requires a list of uris to which the socket should connect to.
+	  *
+	  * \param context A reference to the ZMQ context in which the socket should be created.
+	  * \param uris A reference to a list of strings to which the socket should connect to.
+	  * \param subscribe If set to true, the socket will automatically subscribe to any kind
+	  * of messages. If set to false, no subscription will be done.
+	  */
+	ZMQSubscriberSocket(/** [in] */ zmq::context_t & context, /** [in] */ std::list<std::string> & uris,
+						/** [in] */ bool subscribe = true) {
+		this->socket = new zmq::socket_t(context, ZMQ_SUB);
+		this->connect(uris);
+		if (subscribe)
+			this->subscribe();
+	};
+	/** \brief Subscribe to a specific type of message
+	  *
+	  * Establish a new message filter that will be applied to incoming data. The content of the
+	  * filter is binary data. A zero-length string will subscribe to all data. If the same filter
+	  * is applied multiple times, they will all be treated as separate filters.
+	  *
+	  * \param filter A reference to a string containing the filter data that should be added.
+	  */ 
+	inline void subscribe(/** [in] */ std::string & filter = "") { 
 		this->socket->setsockopt(ZMQ_SUBSCRIBE, filter.c_str(), filter.size()); 
 	};
-	inline void unsubscribe(std::string & filter = "") {
+	/** \brief Unsubscribe to a specific type of message
+	  *
+	  * Remove an existing message filter. If there are multiple equivalent filters applied to the
+	  * socket, only one instance of said filter will be removed.
+	  *
+	  * \param filter A reference to a string containing the filter data that should be removed
+	  */
+	inline void unsubscribe(/** [in] */ std::string & filter = "") {
 		this->socket->setsockopt(ZMQ_UNSUBSCRIBE, filter.c_str(), filter.size());
 	};
 };
 
 class ZMQRequestSocket : public ZMQSendingSocket, public ZMQReceivingSocket {
 	public:
-	ZMQRequestSocket(zmq::context_t & context, std::string & uri = "") { this->socket = new zmq::socket_t(context, ZMQ_REQ); this->connect(uri); };
+	ZMQRequestSocket(zmq::context_t & context, std::string & uri = "") { 
+		this->socket = new zmq::socket_t(context, ZMQ_REQ); 
+		this->connect(uri); 
+	};
 };
 
 class ZMQResponseSocket : public ZMQSendingSocket, public ZMQReceivingSocket {
 	public:
-	ZMQResponseSocket(zmq::context_t & context, std::string & uri = "") { this->socket = new zmq::socket_t(context, ZMQ_REP); this->bind(uri); };
+	ZMQResponseSocket(zmq::context_t & context, std::string & uri = "") { 
+		this->socket = new zmq::socket_t(context, ZMQ_REP); 
+		this->bind(uri); 
+	};
 };
 
 /* Close namespaces */
